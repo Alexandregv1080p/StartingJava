9f512d51e5f1b8bbd5c5e95ba56b31c594466cb8
Alexandregv1080p <alexandregv1999@gmail.com>
2023-02-13 10:33:01 -0300

    first commit

---
+++ src/com/dio/PrimeiroPrograma.java
@@ -0,0 +1,15 @@
+package com.dio;
+
+
+
+public class PrimeiroPrograma {
+
+	public static void main(String[] args) {
+		int a = 10;
+		int b = 20;
+		System.out.println("Ola mundo " + (a + b) );
+		Gato gato = new Gato("Felix","Preto",22);
+		System.out.println(gato);
+	}
+
+}
